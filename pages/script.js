9a2f77812f7daf0722afc449ed31908127c634f0
a22nevan <a22nevan@student.his.se>
2026-04-02 16:36:30 +0200

    Added basic functionality for info box
    Added basic functionality for opening and closing the info box for each page. No actual information is written yet, nor is there any proper CSS for it.

--- pages/script.js
+++ pages/script.js
@@ -1,1 +1,11 @@
 //Write all fun scripts and functions here yippee
+
+function showInfo() {
+    document.getElementById("darkFilter").classList.add("visible");
+    document.getElementById("infoBox").classList.add("visible");
+}
+
+function exitInfo() {
+    document.getElementById("darkFilter").classList.remove("visible");
+    document.getElementById("infoBox").classList.remove("visible");
+}
